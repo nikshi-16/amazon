53b6792bcd31d43c6235a77ae0028500adc5f637
nikshi-16 <nikshithadevadiga76>
2025-03-13 12:09:50 +0530

    16-signin-with-google

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,6 +9,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import CredentialsSignInForm from './credentials-signin-form'
 import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
+import { GoogleSignInForm } from './google-signin-form'
 
 export const metadata: Metadata = {
   title: 'Sign In',
@@ -36,6 +37,10 @@ export default async function SignIn(props: {
         </CardHeader>
         <CardContent>
           <div>
+          <SeparatorWithOr />
+            <div className='mt-4'>
+              <GoogleSignInForm />
+            </div>
             <CredentialsSignInForm />
           </div>
         </CardContent>
