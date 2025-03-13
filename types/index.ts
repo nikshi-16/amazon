cb32f78d94ce43d5b0c31ba7565c14f1ed55174e
nikshi-16 <nikshithadevadiga76>
2025-03-13 19:54:22 +0530

    18-place-order

--- types/index.ts
+++ types/index.ts
@@ -1,27 +1,33 @@
-
-import { ProductInputSchema, OrderItemSchema, CartSchema, ShippingAddressSchema, UserInputSchema, UserSignInSchema, UserSignUpSchema } from "@/lib/validator"
+import { CartSchema, OrderInputSchema, OrderItemSchema, ProductInputSchema, ShippingAddressSchema, UserInputSchema, UserSignInSchema, UserSignUpSchema } from "@/lib/validator"
 import { z } from "zod"
+
 
 export type IProductInput = z.infer<typeof ProductInputSchema>
 
-export type Data = {
+export type Data ={
     users: IUserInput[]
+
     products: IProductInput[]
-    headerMenus: {
-        name: string
-        href: string
+    headerMenus:{
+        name:string
+        href:string
     }[]
-    carousels: {
-        image: string
-        url: string
-        title: string
-        buttonCaption: string
-        isPublished: boolean
+    carousels:{
+        image:string
+        url:string
+        title:string
+        buttonCaption:string
+        isPublished:boolean
     }[]
 }
+export type IOrderInput=z.infer<typeof OrderInputSchema>
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
-export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
 export type IUserInput = z.infer<typeof UserInputSchema>
+export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
+
+
+
+
 export type IUserSignIn = z.infer<typeof UserSignInSchema>
-export type IUserSignUp = z.infer<typeof UserSignUpSchema>
+export type IUserSignUp =z.infer<typeof UserSignUpSchema>
